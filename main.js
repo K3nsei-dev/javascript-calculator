a23f001e6a22e8b42fdcf84466e3ade949b466a3
Jeandre <jeandreross@gmail.com>
2021-06-25 11:26:38 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+let addition = document.getElementsByClassName('addition');
+let subtraction = document.getElementsByClassName('subtraction');
+let division = document.getElementsByClassName('division');
+let multiplication = document.getElementsByClassName('multiplication');
+
+
+
+function addition(a, b){
+    return a + b
+};
+
+function subtraction(a, b){
+    a - b
+}
+
+function division(a, b){
+    a / b
+}
+
+function multiplication(a, b){
+    a * b
+}
+
